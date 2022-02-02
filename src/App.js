369da078e9451f358f9f0183fdf63c97216bda49
0y0 <rigoulet.henri.pierre@gmail.com>
2022-02-02 18:06:50 +0100

    deploy smart contract

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { Router, Route } from "react-router-dom";
 function App() {
   return;
-  <div className="App"></div>;
+  <div className="App">v</div>;
 }
 
 export default App;
