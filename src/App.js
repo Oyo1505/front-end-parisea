975a9b533d35091ce8cf63bf0a3c27c43fcc47f3
minniejung <94359917+minniejung@users.noreply.github.com>
2022-02-02 18:06:24 +0100

    app.js added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import "./App.css";
-import { Router, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import AllPosts from "./components/posts/AllPosts";
+
 function App() {
-  return;
-  <div className="App">HomePAge</div>;
+  return (
+    <>
+      <div className="App">
+        <a href="/">HOME</a>
+        <a href="/posts">POSTS</a>
+        <Routes>
+          <Route path="/posts" element={<AllPosts />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
