ef11691f51c0bba1e0257d0a11e9debf0488e657
Melodie Shahrvari <mshahrvari@gmail.com>
2022-02-03 19:45:24 +0100

    User Login, Info & Profile

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import AllPosts from "./components/posts/AllPosts";
+import UserEdit from "./components/user/UserEdit";
+import React, { useState, useEffect } from "react";
+import Login from "./components/user/Login";
+import UserProfile from "./components/user/UserProfile";
 
 function App() {
   return (
     <>
       <div className="App">
         <a href="/">HOME</a>
+        <Login />
         <a href="/posts">POSTS</a>
+
         <Routes>
           <Route path="/posts" element={<AllPosts />} />
+          <Route path="/users/:id" element={<UserProfile />} />
+          <Route path="/users/edit/:id" element={<UserEdit />} />
         </Routes>
       </div>
     </>
--- src/components/user/UserEdit.jsx
+++ src/components/user/UserEdit.jsx
@@ -1,41 +1,70 @@
 import React, { useRef, useState, useEffect } from "react";
 import "../../App.css";
 import APIHandler from "../../api/APIHandler";
-// import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-const UserEdit = ({ id }) => {
-  const imageRef = useRef();
-  const coverImageRef = useRef();
-  const [user, setUser] = useState(null);
+const UserEdit = () => {
+  const { id } = useParams();
+  const imageRef = useRef("");
+  const coverImageRef = useRef("");
+  const [user, setUser] = useState({
+    name: "",
+    image: "",
+    userName: "",
+    email: "",
+    bio: "",
+    coverImage: "",
+  });
+
+  console.log(id);
+
+  // console.log(user);
 
   useEffect(async () => {
-    const { data } = await APIHandler.get("/api/users/" + id);
-    setUser(data);
+    const { data } = await APIHandler.get("/users/edit/" + id);
+    console.log(data);
+    setUser({
+      name: data.name,
+      image: data.image,
+      userName: data.userName,
+      email: data.email,
+      bio: data.bio,
+      coverImage: data.coverImage,
+    });
   }, [id]);
-
+  console.log(user);
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const formData = new FormData();
     formData.append("image", imageRef.current.files[0]);
-    formData.append("image", coverImageRef.current.files[0]);
+    formData.append("coverImage", coverImageRef.current.files[0]);
     formData.append("name", user.name);
     formData.append("userName", user.userName);
     formData.append("email", user.email);
     formData.append("bio", user.bio);
 
     try {
-      const { data } = await APIHandler.patch(`/api/users/${id}`, formData);
-      console.log("Data >>>>>>>>>>> ", data);
+      const { data } = await APIHandler.patch(`/users/edit/${id}`, formData);
+      console.log("Data >>>>>>>>>>> ", formData);
+      setUser({
+        name: data.name,
+        userName: data.userName,
+        email: data.email,
+        bio: data.bio,
+        image: data.image,
+      });
     } catch (e) {
       console.error(e);
     }
   };
 
+  // useEffect();
+
   return (
     <div>
       <h1>Edit your profile</h1>
-      <form action="">
+      <form>
         <div>
           <h2>Enter your details</h2>
 
@@ -51,13 +80,13 @@ const UserEdit = ({ id }) => {
           </div>
 
           <div>
-            <label htmlFor="username">Username</label>
+            <label htmlFor="userName">Username</label>
             <input
               type="text"
-              name="username"
-              placeholder=""
-              value={user.username}
-              onChange={(e) => setUser({ ...user, username: e.target.value })}
+              name="userName"
+              placeholder="userName"
+              value={user.userName}
+              onChange={(e) => setUser({ ...user, userName: e.target.value })}
             />
           </div>
         </div>
@@ -99,6 +128,7 @@ const UserEdit = ({ id }) => {
 
           <div>
             <input ref={imageRef} name="image" type="file" />
+            {imageRef && <img width="350" src={imageRef} />}
           </div>
         </div>
 
@@ -108,6 +138,7 @@ const UserEdit = ({ id }) => {
 
           <div>
             <input ref={coverImageRef} name="image" type="file" />
+            {coverImageRef && <img width="350" src={coverImageRef} />}
           </div>
         </div>
 
