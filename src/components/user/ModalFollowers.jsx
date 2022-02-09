666fedbd0a169438aeef7dbed6de32ea20414d0a
Melodie Shahrvari <mshahrvari@gmail.com>
2022-02-09 16:00:10 +0100

    style changes

--- src/components/user/ModalFollowers.jsx
+++ src/components/user/ModalFollowers.jsx
@@ -16,25 +16,32 @@ const ModalFollowers = ({ open, users, onCloseModal }) => {
           modal: "customModal",
         }}
       >
-        <ul>
-          {users.map((e) => (
-            <li>
-              <Link to={`/profile/${e._id}`} onClick={onCloseModal}>
-                <div className="modal-list">
-                  <div className="modal-list-user">
-                    <div className="image-modal">
-                      <img className="img-modal-user" src={e.image} alt="" />
-                    </div>
-                    <div className="name-username">
-                      <span className="name-in-modal">{e.name}</span>
-                      <span className="username-in-modal">@{e.userName}</span>
+        {users.length === 0 ? (
+          <di className="modal-list-empty">
+            <p>Spread Love and NFTs</p>
+            <p>Follow amazing artists now !</p>
+          </di>
+        ) : (
+          <ul>
+            {users.map((e) => (
+              <li>
+                <Link to={`/profile/${e._id}`} onClick={onCloseModal}>
+                  <div className="modal-list">
+                    <div className="modal-list-user">
+                      <div className="image-modal">
+                        <img className="img-modal-user" src={e.image} alt="" />
+                      </div>
+                      <div className="name-username">
+                        <span className="name-in-modal">{e.name}</span>
+                        <span className="username-in-modal">@{e.userName}</span>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </Link>
-            </li>
-          ))}
-        </ul>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
       </Modal>
     </div>
   );
--- src/components/user/UserProfile.jsx
+++ src/components/user/UserProfile.jsx
@@ -9,19 +9,33 @@ import "../../assets/css/user/user-content.css";
 
 const UserProfile = () => {
   const { currentUser, coverImage } = useAuth();
+  console.log("ici ++++++", currentUser);
 
   const [mode, setMode] = useState("creator");
   // if (currentUser.length === 0) return <p>loading</p>;
   return (
     <div className="body-profile">
       {/* <NavLink to={"/posts"}>Posts</NavLink> */}
-      <NavLink to={"/w"}>WishList</NavLink>
-
-      <UserInfo />
-      <span onClick={() => setMode("creator")}>Creator</span>
-      <span onClick={() => setMode("owner")}>Owner</span>
-      <span onClick={() => setMode("posts")}>Posts</span>
-      <ListNftsUserProfile mode={mode} userId={coverImage.id} />
+      <div className="profile-image-header">
+        <img src={coverImage.image} />
+      </div>
+      {/* <NavLink to={"/w"}>WishList</NavLink> */}
+      <div className="body-profile-bis">
+        <UserInfo />
+        <div className="side-elements-profile">
+          <div className="whole-cat-profile">
+            <div className="categories-profile">
+              <p onClick={() => setMode("creator")}>
+                Created
+                {/* {currentUser[0].nfts_ids_created.length} */}
+              </p>
+              <p onClick={() => setMode("owner")}>Owned</p>
+              <p onClick={() => setMode("posts")}>Posts</p>
+            </div>
+          </div>
+          <ListNftsUserProfile mode={mode} userId={coverImage.id} />
+        </div>
+      </div>
     </div>
   );
 };
